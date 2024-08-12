359d0a33f9bd1828538edbb0b657171f94230272
karishma-negi <karishmanegi159@gmail.com>
2024-08-12 18:50:56 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-pytest -v -s --html=HTMLReports/myhtmlreport_headless.html --alluredir="allure-results"
+C:\Users\USER\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s --html=HTMLReports/myhtmlreport_headless.html --alluredir="allure-results"
